7736b0c32743b1de96e1b854b654ac3b55074f27
Aulis Leinonen <aulis.leinonen@student.tut.fi>
2018-05-19 16:47:53 +0300

    Removed unnecessary logs

--- src/components/Placeholder.vue
+++ src/components/Placeholder.vue
@@ -1,7 +1,24 @@
 <template>
+  <MaterialAccordion
+  :materialApi="materialApi"/>
 </template>
 
 <script>
+import MaterialAccordion from 'cloubi2-default-product-theme-components-vue/src/components/MaterialAccordion'
+
+export default {
+
+  name: "Placeholder",
+  
+  components: { MaterialAccordion },
+
+  props: {
+    materialApi: { type: Object, required: true  }
+  },
+
+  created: function(){
+  }
+}
 </script>
 
 <style>
--- src/script.js
+++ src/script.js
@@ -2,18 +2,24 @@ import setUpPublicPath from './public-path'
 import './style.scss'
 import Vue from 'vue'
 
-import Placeholder from './components/Placeholder.vue'
+import { MaterialApiWrapper } from 'cloubi2-default-product-theme-components-vue';
+import Placeholder from './components/Placeholder';
 
 setUpPublicPath.then(() => {
 
     Cloubi.load(['fi.cloubi.frontend/material', 'fi.cloubi.frontend/settings'], function(material, settings) {
+        
+        const materialApi = new MaterialApiWrapper(material);
 
         // Wait until material is fully loaded and we know what the current page is.
         material.onMaterialReady(function() {
             if (settings.frameEnabled) {
-                // If frame is enabled, show it 
-               
-            }
+              // If frame is enabled, show it
+
+            new Vue({
+              el: document.getElementById('top-content'),
+              render: h => h(Placeholder, {props: {materialApi:materialApi}})
+            });
 
             // The blank navigation model does not load current page automatically,
             // in case we wanted to add some custom rendering logic to it.
@@ -21,6 +27,7 @@ setUpPublicPath.then(() => {
             material.getCurrentPage(function(page) {
                 material.changePage(page.id);
             });
+          }
         });
     });
 
