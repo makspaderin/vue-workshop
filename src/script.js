4d5ac43814a5542d4bd6fc46a82723a56b14d348
Anssi Penttila <anssi.penttila@prezella.com>
2018-05-21 16:41:20 +0300

    menu

--- src/script.js
+++ src/script.js
@@ -3,6 +3,8 @@ import './style.scss'
 import Vue from 'vue'
 
 import { MaterialApiWrapper } from 'cloubi2-default-product-theme-components-vue';
+import MainMenuPage from 'cloubi2-default-product-theme-components-vue/src/components/MainMenuPage'
+
 import Placeholder from './components/Placeholder';
 
 setUpPublicPath.then(() => {
@@ -11,6 +13,23 @@ setUpPublicPath.then(() => {
         
         const materialApi = new MaterialApiWrapper(material);
 
+        material.registerPageContentTypeRenderer('navigation/menu', function(page, contentId, callback) {
+          
+          console.log('navigation/menu render =>');
+
+          let mainMenu = new Vue({
+            render: h => h(MainMenuPage, { props: {pageId: page.id, pageTitle: page.title, materialApi} })
+          });
+    
+          mainMenu.$mount();
+    
+          var content = document.getElementById(contentId);
+          content.appendChild(mainMenu.$el);
+    
+          callback();
+         
+        });
+    
         // Wait until material is fully loaded and we know what the current page is.
         material.onMaterialReady(function() {
             if (settings.frameEnabled) {
