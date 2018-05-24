54a52af7dc2d57c08bb0b3fe42e188cbc7c060a1
Aulis Leinonen <aulis.leinonen@student.tut.fi>
2018-05-24 10:28:02 +0300

    Changed topbar to override the default topbar content

--- src/components/ThemeTopbar.vue
+++ src/components/ThemeTopbar.vue
@@ -1,7 +1,16 @@
 <template>
   <navbar
-    :event-bus="eventBus"
-    :material-api="materialApi">
+    :is-transparent="isRoot">
+    <template slot="left-content">
+      <sidebar-switch 
+        :event-bus="eventBus" />
+      <home-button
+        :material-api="materialApi" />
+    </template>
+    <template slot="center-content">
+      <product-heading
+        :material-api="materialApi" />
+    </template>
     <template slot="right-content">
       Topbar...
       <ruler-button 
@@ -22,6 +31,32 @@ export default {
   props: {
     materialApi: { type: Object, required: true },
     eventBus: { type:Object, required: true }
+  },
+
+  data: function(){
+    return {
+      isRoot: false
+    }
+  },
+  
+  created: function() {
+    const self = this;
+
+    self.materialApi.onPageChange(self.$pageChanged);
+    self.materialApi.getCurrentPage().then((page) => {
+      self.$pageChanged(page);
+    });
+  },
+
+  methods: {
+    $pageChanged: function(page) {
+      if(page.breadcrump.length === 1){
+        this.isRoot = true;
+      }
+      else {
+        this.isRoot = false;
+      }
+    }
   }
 
 }
