6127eadfd8ed867b5685c0a36de07d7f8b69678b
AulisL <aulis.leinonen@prezella.com>
2018-06-02 09:56:04 +0300

    Added more detailed tests for notes

--- cypress/integration/default-theme.js
+++ cypress/integration/default-theme.js
@@ -125,7 +125,7 @@ describe('PageTurner', function(){
 
     cy.get('[data-cy=cb-content-pos]').find('[data-cy=page-turner-prev]').should('not.be.visible')
   })
-})
+});
 
 describe('Notes', function(){
   beforeEach(function() {
@@ -161,6 +161,69 @@ describe('Notes', function(){
   });
   
   it('Deletes a note', function() {
+    /* Cancel delete */
 
+    cy.get('[data-cy=notes-list-page]').children().first().trigger('mouseover');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=delete-note-button]').click();
+    
+    cy.get('[data-cy=note-dialog-delete]').find('[data-cy=cancel-button]').click();
+    cy.get('[data-cy=note-dialog-delete]').should('not.be.visible');
+
+    /* Check nothing has been deleted */
+
+    cy.get('[data-cy=notes-list-page]').children().should('have.length', 1);
+
+    cy.get('[data-cy=note-type-selector-all]').click();
+    cy.get('[data-cy=notes-list-all]').children().should('have.length', 2);
+    cy.get('[data-cy=note-type-selector-page]').click();
+
+    /* Delete properly */
+
+    cy.get('[data-cy=notes-list-page]').children().first().trigger('mouseover');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=delete-note-button]').click();
+
+    cy.get('[data-cy=note-dialog-delete]').find('[data-cy=delete-button]').click();
+    cy.wait(1200)
+    cy.get('[data-cy=note-dialog-delete]').should('not.be.visible');
+    cy.get('[data-cy=notes-list-page]').children().should('have.length', 0);
+
+    cy.get('[data-cy=note-type-selector-all').click();
+    cy.get('[data-cy=notes-list-all]').children().should('have.length', 1);
+  });
+
+  it('Edit a note', function(){
+
+    /* Cancel edit */
+
+    cy.get('[data-cy=notes-list-page]').children().first().trigger('mouseover');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=edit-note-button]').click();
+
+    cy.get('[data-cy=note-dialog-edit').find('[data-cy=note-text-input]').find('textarea').clear();
+
+    cy.get('[data-cy=note-dialog-edit]').find('[data-cy=cancel-button]').click();
+    cy.get('[data-cy=note-dialog-edit]').should('not.be.visible');
+
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=note-text]').contains('Note 1');
+
+    /* Edit properly */
+
+    
+    cy.get('[data-cy=notes-list-page]').children().first().trigger('mouseover');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=edit-note-button]').click();
+
+    cy.get('[data-cy=note-dialog-edit').find('[data-cy=note-text-input]').find('textarea').type('2');
+
+    cy.get('[data-cy=note-dialog-edit]').find('[data-cy=save-button]').click();
+    
+    cy.wait(1200);
+
+    cy.get('[data-cy=note-dialog-edit]').should('not.be.visible');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=note-text]').contains('Note 12');
+    cy.get('[data-cy=notes-list-page]').children().first().find('[data-cy=note-time]').contains('just-now');
+
+    /* Check all notes too */
+
+    cy.get('[data-cy=note-type-selector-all').click();
+    cy.get('[data-cy=notes-list-all]').children().first().find('[data-cy=note-text]').contains('Note 12');
   });
 });
